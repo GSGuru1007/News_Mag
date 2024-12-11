ce59c9a05f5e2fa48a2fe976371ef91733e6110f
GSGuru1007 <gsguru0710@gmail.com>
2024-12-11 23:53:34 +0530

    update .gitignore to include .env

--- src/components/NewsBord.jsx
+++ src/components/NewsBord.jsx
@@ -5,17 +5,22 @@ export const NewsBord = ({category}) => {
   const [articles,setArticles] = useState([]);
   
   useEffect(()=>{
-    let url = ` https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
-    fetch(url).then(response => response.json()).then(data => setArticles(data.articles));
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    fetch(url)
+  .then(response => response.json())
+  .then(data => setArticles(data.articles || []))
+  .catch(error => console.error('Error fetching news:', error));
+
   },[category])
 
 
   return (
     <div >
       <h2 className="text-center mt-5 " style={{ marginTop: '5px', padding:'10px'}}>Latest <span className="badge bg-danger">News</span> </h2>
-      {articles.map((news,index)=>{
-          return <Newsitem key={index} title ={news.title} description={news.description} src={news.urlToImage}  url={news.url}/>
-      })}
+      {articles?.length > 0 ? articles.map((news, index) => (
+    <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+)) : <p>No news available.</p>}
+
 
     </div>
   )
